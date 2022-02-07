09c2c538a48e494d45e396b6209493747c630d94
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-02-07 12:56:54 +0700

    feat: render component with config route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React, { Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import * as serviceWorker from './serviceWorker';
 
-import 'antd/dist/antd.css';
 import './index.css';
-
-import Footer from './app/components/Footer';
+import 'antd/dist/antd.css';
+import { history, renderRouteConfigs, routes } from 'app/config/router';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<h1>Hello World</h1>
-		<Footer />
+		<Suspense fallback={<p>Loading ...</p>}>
+			<BrowserRouter history={history}>{renderRouteConfigs(routes)}</BrowserRouter>
+		</Suspense>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
