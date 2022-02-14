37aafda94e0ed6db1bd9dfe3c0e7baedcca2010d
Raymond <96218923+raymond-le-goldenowl@users.noreply.github.com>
2022-02-14 15:54:49 +0700

    feat: build appbar ui (#4)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.css';
+
+import AppBar from 'app/components/AppBar';
+import './index.css';
+
+import * as serviceWorker from './serviceWorker';
 
 import Footer from './app/components/Footer';
 
 ReactDOM.render(
 	<React.StrictMode>
+		<AppBar />
 		<h1>Hello World</h1>
 		<Footer />
 	</React.StrictMode>,
