d957f645e0fbb755d7f5f1181ad7cd72d09c9fb2
Raymond <96218923+raymond-le-goldenowl@users.noreply.github.com>
2022-02-21 09:16:18 +0700

    feat: build custom select option (#5)
    * fix: create select option ui
    
    * feat: build custom select option
    
    * fix: remove font weight with wrong value
    
    * fix: change component named inappropriate

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-
-import AppBar from 'app/components/AppBar';
-import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
-import Footer from './app/components/Footer';
+import SelectOption from 'app/components/SelectOption';
+
+import 'antd/dist/antd.css';
+import './index.css';
+
+// demo data
+const options = [
+	{ key: 1, value: 1, text: 'Text 1' },
+	{ key: 2, value: 2, text: 'Text 2' },
+	{ key: 3, value: 3, text: 'Text 3' },
+	{ key: 4, value: 4, text: 'Text 4' },
+	{ key: 5, value: 5, text: 'Text 5' },
+	{ key: 6, value: 6, text: 'Text 6' }
+];
 
 ReactDOM.render(
 	<React.StrictMode>
-		<AppBar />
 		<h1>Hello World</h1>
-		<Footer />
+
+		{/* demo render custom */}
+		<SelectOption
+			options={options}
+			width={'50%'}
+			placeholder={'Select option'}
+			defaultValue={'1'}
+		/>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
