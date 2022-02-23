a9114ceee588da0e5b26e27c3ccf09331762884c
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-02-23 14:18:12 +0700

    fix: clearn src/index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import Footer from './app/components/Footer';
-
 import './index.scss';
 import 'antd/dist/antd.css';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<h1>Hello World</h1>
-		<Footer />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+ReactDOM.render(<React.StrictMode></React.StrictMode>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
