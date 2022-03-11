d13144a0687afa213a8bf1770f3f559bb000c1ef
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-03-11 15:59:22 +0700

    config route for history

--- src/app/config/router/index.js
+++ src/app/config/router/index.js
@@ -4,12 +4,21 @@ import { nanoid } from 'nanoid';
 import { createBrowserHistory } from 'history';
 import { Navigate, Route, Routes } from 'react-router';
 
-const DeclarationForm = lazy(() => import('../../pages/DeclarationForm'));
-const DeclarationFormWrapper = lazy(() => import('../../Layouts/declarationFormWrapper'));
+const HistoryPage = lazy(() => import('app/pages/HistoryPage'));
+const DeclarationForm = lazy(() => import('app/pages/DeclarationForm'));
+const DeclarationFormWrapper = lazy(() => import('app/Layouts/declarationFormWrapper'));
 
 export const history = createBrowserHistory();
 
 export const routes = {
+	historyPage: {
+		id: nanoid(4),
+		path: '/history',
+		exact: true,
+		component: HistoryPage,
+		layout: DeclarationFormWrapper,
+		content: ' Declaration History'
+	},
 	declarationForm: {
 		id: nanoid(4),
 		path: '/declaration-form',
